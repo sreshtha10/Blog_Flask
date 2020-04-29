4dd9d78972038a17fe073a4a555477166f0b3351
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-30 00:28:59 +0530

    main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash, session, request, redirect
 from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_ckeditor import CKEditor
 import yaml
 import os
 
@@ -18,11 +19,20 @@ blog.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(blog)
 blog.config['SECRET_KEY'] = os.urandom(24)
 
+CKEditor(blog)
+
 
 # Home Page
 @blog.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute('SELECT * FROM blog')
+    if resultValue > 0:
+        blogs = cur.fetchall()
+        cur.close()
+        return render_template('index.html', blogs=blogs)
+    cur.close()
+    return render_template('index.html', blogs=None)
 
 
 # Registration Page
@@ -47,7 +57,7 @@ def register():
 # Login Page
 @blog.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method =='POST':
+    if request.method == 'POST':
         userDetails = request.form
         username = userDetails['username']
         cur = mysql.connection.cursor()
@@ -70,6 +80,33 @@ def login():
         cur.close()
         return redirect('/')
     return render_template('login.html')
+
+
+# Writing the blog
+@blog.route('/write-blog/', methods=['GET', 'POST'])
+def write():
+    if request.method == 'POST':
+        blogspot = request.form
+        title = blogspot['title']
+        body = blogspot['body']
+        author = session['first_name'] + ' ' + session['last_name']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO blog(title,body,author) VALUES(%s,%s,%s)', (title, body, author))
+        mysql.connection.commit()
+        cur.close()
+        flash('Blog successfully posted !', 'success')
+        return redirect('/')
+    return render_template('write-blog.html')
+
+
+@blog.route('/blogs/<int:id>/')
+def blogs(id):
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute('SELECT * FROM blog WHERE blog_id = {}'.format(id))
+    if resultValue > 0:
+        blog = cur.fetchone()
+        return render_template('blogs.html', blog=blog)
+    return 'Blog not found'
 
 
 if __name__ == '__main__':
